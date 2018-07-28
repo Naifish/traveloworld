b0d6c6c9f8b6b3e394987aeee1de665531c3d1d0
Unknown <fr894333@dal.ca>
2018-07-28 01:39:12 -0300

    Hotel UI Flow
    -atlantic hotel ui completed

--- atlantic/index.php
+++ atlantic/index.php
@@ -1,21 +1,92 @@
 <?php
 
 session_start();
+if(isset($_SESSION) && !empty($_SESSION['email'])){ header('location:index.php');}
+$startDate='';
+$endDate='';
+$minAmt='';
+$maxAmt='';
+$errs=array();
 
+
+if (isset($_POST['btn-login'])) {
+  # code...
+  require 'includes/database.php';
+  if (empty($_POST['startDate'])) {
+    $errs[]="Start Date is required";
+  }else{
+    $startDate=$_POST['startDate'];
+  }
+
+  if (empty($_POST['endDate'])) {
+    $errs[]="End Date is required";
+  }else{
+    $endDate=$_POST['endDate'];
+  }
+
+  if (empty($_POST['minAmt'])) {
+    $errs[]="Minimum Amount is required";
+  }else{
+    $minAmt=$_POST['minAmt'];
+  }
+
+  if (empty($_POST['maxAmt'])) {
+    $errs[]="Maximum Amount is required";
+  }else{
+    $maxAmt=$_POST['maxAmt'];
+  }
+}  
 ?>
 <!DOCTYPE html>
 <html>
 <?php 
 require 'includes/header.php';
 ?>
-
+<center>
+	<h1>Welcome to Atlantic Hotel</h1>
+	<p>Book a room at a reasonalble price</p>
+</center>
 <div class="container container-registration">
 <div class="row">
-	<div class="col-md-12">
-		Hello World
-	</div>
+	
+<form action="list-rooms.php?startDate=<?php echo $startDate;?>&endDate=<?php echo $endDate;?>&minAmt=<?php echo $minAmt;?>&maxAmt=<?php echo $maxAmt;?>" method="GET">
+  <h3 class="text-center">Search a room</h3>
+  <ul>
+    <?php if (count($errs)>0){ foreach ($errs as $er) {
+    ?>
+    <li><?php echo $er; ?></li>
+    <?php }} ?>
+  </ul>
+  <div class="form-group col-md-6">
+    <label >Start Date</label>
+    <input type="date" class="form-control" name="startDate" placeholder="Pick a date" required>
+    <!-- <small>Your email id is safe with us.</small> -->
+  </div>
+  <div class="form-group col-md-6">
+    <label >End Date</label>
+    <input type="date" class="form-control" name="endDate" placeholder="Pick a date" required>
+  </div>
+
+  <div class="form-group col-md-6">
+    <label >Minumum Amount</label>
+    <input type="text" class="form-control" name="minAmt" placeholder="Enter minimum amount" required>
+    <!-- <small>Your email id is safe with us.</small> -->
+  </div>
+  <div class="form-group col-md-6">
+    <label >Maximum Amount</label>
+    <input type="text" class="form-control" name="maxAmt" placeholder="Enter maximum amount" required>
+  </div>  
+
+
+  <input type="submit" value="Submit" name="btn-login" class="btn btn-success">
+</form>
 </div>
 </div>
+<!-- <img src="images/atlantic.jpg" style="width: 100%; height: 520px;"> -->
+
+
+
+
 <?php 
 include 'includes/footer.php';
 ?>
--- atlantic/list-rooms.php
+++ atlantic/list-rooms.php
@@ -12,6 +12,21 @@ require 'includes/header.php';
 	<ol></ol>
 </div>
 
+<?php
+$startDate = $_GET['startDate']; 
+$endDate = $_GET['endDate']; 
+$minAmt = $_GET['minAmt']; 
+$maxAmt = $_GET['maxAmt']; 
+?>
+<form>
+    <input type="hidden" name="sendId" id="startDate" value="<?php echo $startDate;?>">
+    <input type="hidden" name="sendId" id="endDate" value="<?php echo $endDate;?>">
+    <input type="hidden" name="sendId" id="minAmt" value="<?php echo $minAmt;?>">
+    <input type="hidden" name="sendId" id="maxAmt" value="<?php echo $maxAmt;?>">
+
+</form>
+
+
 <?php 
 include 'includes/footer.php';
 ?>
@@ -20,8 +35,20 @@ include 'includes/footer.php';
 <script type="text/javascript">
 	
 $(document).ready(function() {
+var startDate = document.getElementById('startDate').value; 
+var endDate = document.getElementById('endDate').value; 
+var minAmt = document.getElementById('minAmt').value; 
+var maxAmt = document.getElementById('maxAmt').value; 
+
+console.log(startDate);
+console.log(endDate);
+console.log(minAmt);
+console.log(maxAmt);
+
+
+
 $.ajax({
-    url: "http://localhost/traveloworld/atlantic/public/index.php/rooms/all",
+    url: "http://localhost/traveloworld/atlantic/public/index.php/rooms/all/"+startDate+"/"+endDate+"/"+minAmt+"/"+maxAmt,
     method: 'GET',
     contentType: 'application/json',
     dataType: 'JSON',
@@ -35,9 +62,11 @@ Available: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_html_ap
         var roomType = data[i].roomType;
         var description = data[i].description;
         var room = data[i].roomNumber;
+        var id= data[i].id;
+        var price= data[i].price;
         var image = '<img src="https://res.cloudinary.com/drtnvyqg9/image/upload/w_250,bo_1px_solid_rgb:00390b,f_png,c_fill/'+data[i].image+'">';
         
-        $("ol").append("<li><div class='container list-room'><div class='row'><div class='col-md-3'>"+image+"</div><div class='col-md-7'><h3>Room Type"+roomType+"<hr/><p>"+description+"</p><p>"+room+"</p></div><div class='col-md-2'><h2>CAD 300</h2><a href='#' class='btn-primary btn-rooms'>Book this Room</a></div></div></div></li><br />");
+        $("ol").append("<li><div class='container list-room'><div class='row'><div class='col-md-3'>"+image+"</div><div class='col-md-7'><h3>Room Type"+roomType+"<hr/><p>"+description+"</p><p>"+room+"</p></div><div class='col-md-2'><h2>CAD"+price+"</h2><a href='single-room.php?id="+id+"&startDate="+startDate+"&endDate="+endDate+"' class='btn-primary btn-rooms'>Book this Room</a></div></div></div></li><br />");
     }    
                     
     } 
